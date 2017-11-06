71c9e9428bc16143e84338ec27f44ca79637b3d8
Sk4nd <manuel.ciangottini@gmail.com>
2017-11-06 13:35:47 +0100

    fixed project dependencies with new unity version

---
+++ Assets/TurnOffVuforia.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TurnOffVuforia : MonoBehaviour {
+
+    void Start()
+    {
+        setVuforiaOff();
+    }
+
+    private void setVuforiaOff()
+    {
+        //Turn off Vuforia
+        VuforiaBehaviour.Instance.enabled = false;
+    }
+}
---
+++ Assets/TurnOnVuforia.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TurnOnVuforia : MonoBehaviour {
+
+    void Start()
+    {
+        setVuforiaOn();
+    }
+
+    private void setVuforiaOn ()
+    {
+        //Turn on Vuforia
+        VuforiaBehaviour.Instance.enabled = true;
+    }
+
+}
